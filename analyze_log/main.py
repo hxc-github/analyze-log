6dcc9f821a389d6af14dd8faf0246807438d882a
hxc-github <1076707510@qq.com>
2019-10-13 01:00:48 +0800

    [ADD]添加完整报表

--- analyze_log/main.py
+++ analyze_log/main.py
@@ -6,13 +6,16 @@ import report
 def main():
 
     parser_log = utils.Parser()
-    logs = parser_log.filter_log_by_type('test.txt')
+    logs = parser_log.filter_log_by_type('../test.txt')
 
     article_reports = report.ArticleReports(logs)
     article_reports.article_report()
 
     ip_reports = report.IPReports(logs)
     ip_reports.ip_report()
+
+    complete_reports = report.CompleteReports(logs)
+    complete_reports.complete_report()
 
 
 if __name__ == '__main__':
--- analyze_log/report.py
+++ analyze_log/report.py
@@ -6,6 +6,7 @@ ARTICLE_TYPES = ['htm', 'html', 'pdf', 'doc', 'docx']
 class ReportBase(object):
 
     def __init__(self, log_list):
+        # 列表存放读取的单行日志，为字典结构[{'url':xxx},]
         self.log_list = log_list
 
 
@@ -21,7 +22,6 @@ class ArticleReports(ReportBase):
         :return:
         """
         # 通过遍历日志列表，按照文章类型进行筛选
-        # 列表存放读取的单行日志，为字典结构
         report_dict = dict()
         for log in self.log_list:
             url = log.get('url')
@@ -72,7 +72,6 @@ class IPReports(ReportBase):
         :return:
         """
         # 通过遍历日志列表，按照IP进行筛选
-        # 列表存放读取的单行日志，为字典结构
         report_dict = dict()
         for log in self.log_list:
             ip = log.get('ip')
@@ -105,3 +104,42 @@ class IPReports(ReportBase):
             access_count = info.get('access_count', None)
             article_count = info.get('article_count', None)
             print "|{}|{}|{}|".format(ip, access_count, article_count)
+
+
+# 完整报表
+class CompleteReports(ReportBase):
+
+    def __init__(self, log_list):
+        super(CompleteReports, self).__init__(log_list=log_list)
+
+    def _collect_complete_reports(self):
+        """
+            收集完整报表
+        :return:
+        """
+        # 通过遍历日志列表，按照IP和URL进行筛选
+        report_dict = dict()
+        for log in self.log_list:
+            ip = log.get('ip')
+            url = log.get('url')
+            ip_url = ip.join(url)
+
+            # 如果字典里没有包含该IP，则创造个新的字典
+            if ip_url in report_dict:
+                    report_dict[ip_url]['access_count'] += 1
+            else:
+                # 构造report为{ip_url:{'access_count':xxx}}
+                new_report = {'ip': ip, 'url': url, 'access_count': 1}
+                report_dict.update({ip_url: new_report})
+
+        return report_dict
+
+    def complete_report(self):
+        report_dict = self._collect_complete_reports()
+        print "|IP|URL|访问次数|"
+        print "|----|----|------|"
+        for ip_url, info in report_dict.iteritems():
+            ip = info.get('ip', None)
+            url = info.get('url', None)
+            access_count = info.get('access_count', None)
+            print "|{}|{}|{}|".format(ip, url, access_count)
